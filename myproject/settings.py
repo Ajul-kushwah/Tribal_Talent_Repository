bf200cb365ada936e878843da26347d761341eda
Ajul Kushwah <48391215+Ajul-kushwah@users.noreply.github.com>
2021-01-17 14:48:13 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u#hf3!f2a@+agh^$=(!91*wi2okc2dp6-blj(9xx*et61b*cr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
